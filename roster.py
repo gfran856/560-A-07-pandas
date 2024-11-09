b426adc4a7a399eb7960b6cba4f5c86a92fdb3db
Grey.Francisco <gfran856@unc.edu>
2024-11-09 13:46:46 -0500

    0 header

--- roster.py
+++ roster.py
@@ -1,10 +1,15 @@
 # Mens Basketball Roster --> https://goheels.com/sports/mens-basketball/roster
 
+# Import Pandas
+import pandas as pd
+
 # Create the list of the different players (I choose to do 14 instead of at least 10)
 players = ['Claude', 'Brown', 'Cadeau', 'Davis',
             'Tyson', 'Davis', 'Trimble', 'Powell',
               'Jackson', 'Washington', 'Hawkins', 'Holbrook',
                 'Lubin', "Withers", 'Mayo Jr.']
-# Create the For loop
+data = pd.DataFrame(players)
+print(data)
+'''# Create the For loop
 for player in players:
-    print(players)
+    print(players)'''
