d08488430eaac58af6782af1fb40802d75eca516
Grey.Francisco <gfran856@unc.edu>
2024-11-09 12:36:37 -0500

    roster list

---
+++ roster.py
@@ -0,0 +1,8 @@
+# Mens Basketball Roster --> https://goheels.com/sports/mens-basketball/roster
+
+# Create the list of the different players (I choose to do 14 instead of at least 10)
+players = ['Claude', 'Brown', 'Cadeau', 'Davis',
+            'Tyson', 'Davis', 'Trimble', 'Powell',
+              'Jackson', 'Washington', 'Hawkins', 'Holbrook',
+                'Lubin', "Withers", 'Mayo Jr.']
+print(players)
