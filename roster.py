f303a5479203a93d2eccfbed1f043817076ac51e
Grey.Francisco <gfran856@unc.edu>
2024-11-27 13:52:27 -0500

    Remove roster variable

--- roster.py
+++ roster.py
@@ -4,14 +4,12 @@
 import pandas as pd
 
 # Create the list of the different players (I choose to do 14 instead of at least 10)
-roster = ['Claude', 'Brown', 'Cadeau', 'Davis',
-            'Tyson', 'Davis', 'Trimble', 'Powell',
-              'Jackson', 'Washington', 'Hawkins', 'Holbrook',
-                'Lubin', "Withers", 'Mayo Jr.']
-player = {"Last Name": roster,
+
+player = {"Last Name": ['Claude', 'Brown', 'Cadeau', 'Davis', 'Tyson', 'Davis', 'Trimble', 'Powell', 'Jackson', 'Washington', 'Hawkins', 'Holbrook', 'Lubin', "Withers", 'Mayo Jr.'],
           "First Initial": ["T.", "J.", "E.", "R.", "C.", "E.", "S.", "D.", "I.", "J.", "R.", "J.", "V.", "J.", "D."],
           "Height (inches)": [79, 82, 73, 72, 79, 75, 75, 78, 76, 82, 73, 80, 80, 81, 73],
-          "Weight (pounds)": [230, 215, 180, 180, 200, 215, 195, 195, 190, 235, 175, 230, 230, 220, 175,]}
+          "Weight (pounds)": [230, 215, 180, 180, 200, 215, 195, 195, 190, 235, 175, 230, 230, 220, 175,]
+          }
 
 data = pd.DataFrame(player)
 print(data)
